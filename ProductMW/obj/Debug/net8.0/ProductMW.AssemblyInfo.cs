5998cbdb8e1ddbab502dce4f384d57b00d4c3af1
Ivan Bulaš <ivanbulas@yahoo.com>
2024-12-13 23:55:39 +0100

    /new commit

--- ProductMW/obj/Debug/net8.0/ProductMW.AssemblyInfo.cs
+++ ProductMW/obj/Debug/net8.0/ProductMW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductMW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f045c9816ce395017541eba26f97a3a9f43561ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e842714b3ac82f29325e44abab442f3ace6518e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductMW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductMW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
